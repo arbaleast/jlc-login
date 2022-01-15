1ce9617a7c23c5d8faeec1e072db4e92107f3695
arbaleast <3309839520@qq.com>
2022-01-15 16:40:45 +0800

    update sys_type file

--- system_type.py
+++ system_type.py
@@ -8,7 +8,7 @@ def system():
             path = re.split(r'\n', tmp.read())[1]
             return path
         case 'Linux':
-            path = re.split(r'\n', tmp.read())[1]
+            path = re.split(r'\n', tmp.read())[0]
             return path
         case _:
             print('other platform?')
